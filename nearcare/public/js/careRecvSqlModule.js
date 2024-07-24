5e39c2ee94cc7df5d4379d054e818c391e3a11a7
ain-0308 <172338794+ain-0308@users.noreply.github.com>
2024-07-24 11:58:17 +0900

    아인_240724_11:56 라우터 모듈 코드 수정

--- nearcare/public/js/careRecvSqlModule.js
+++ nearcare/public/js/careRecvSqlModule.js
@@ -2,4 +2,11 @@ const careRecvListSql = function careRecvListSql(){
     let sql = 'select care_receiver_name, care_receiver_birth, care_receiver_gender, care_receiver_phone, user_email, care_receiver_level, care_receiver_add from TB_USER as u inner join TB_CARE_RECEIVER as r where u.user_id = r.user_id';
     return sql;
 };
-module.exports = careRecvListSql;
+const careRecvRegconfrm = function careRecvRegconfrm(){
+    let user_id = 'user001';
+    let sql = `select * from TB_CARE_RECEIVER as r left outer join  TB_USER as u  on u.USER_ID = "${user_id}"and r.USER_ID = "${user_id}" `;
+    return sql;
+};
+
+
+module.exports = {careRecvListSql, careRecvRegconfrm};
--- nearcare/routes/careRecvRegRouter.js
+++ nearcare/routes/careRecvRegRouter.js
@@ -3,30 +3,34 @@ const router = express.Router();
 const conn = require('../config/db');
 const moment = require('moment');
 const maskDatas = require('../public/js/careRecvModule');
-const careRecvListSql = require('../public/js/careRecvSqlModule');
+const sqlModule = require('../public/js/careRecvSqlModule');
+const formetDate = require('../public/js/formatDate');
 // const momentKo = require("moment/locale/ko");
 
 // 요양정보등록 라우터
 router.get('/careRecvRegconfrm', (req, res)=>{
-    let sql = 'select * from TB_CARE_RECEIVER as r left outer join  TB_USER as u  on u.USER_ID = "?"and r.USER_ID = "?" ';
+    // let sql = 'snpmSER_ID = "?"and r.USER_ID = "?" ';
+    let sql = sqlModule.careRecvRegconfrm();
     // sql 임시 정보 불러오기 위해 id 지정
     // 추후 선택한 대상자 정보 불러와 넣을 예정-아인
-    let user_id = 'user001';
-    conn.query(sql, [user_id , user_id], (err, rows)=>{
+    // let user_id = 'user001';
+    conn.query(sql,  (err, rows)=>{
         // 날짜 데이터 변환
-        let birthDay = moment(rows[0].care_receiver_birth);
-        let btd_set = birthDay.format('YYYY-MM-DD');
-        let btd_split = btd_set.split('-');
-        let btDay = btd_split[0]+'년 '+btd_split[1] + '월 ' + btd_split[2] + '일';
-
+        // let birthDay = moment(rows[0].care_receiver_birth);
+        // let btd_set = birthDay.format('YYYY-MM-DD');
+        // let btd_split = btd_set.split('-');
+        // let btDay = btd_split[0]+'년 '+btd_split[1] + '월 ' + btd_split[2] + '일';
+        let birthDay = rows[0].care_receiver_birth;
+        let btDay = formetDate(birthDay);
         res.render('careReceiverReg', {dbData : rows[0], btDay : btDay});
     });
 });
 
 // 요양 대상자 리스트 조회해와 마스킹 처리
 router.get('/careRecvList', (req, res)=>{
-    let sql = 'select care_receiver_name, care_receiver_birth, care_receiver_gender, care_receiver_phone, user_email, care_receiver_level, care_receiver_add from TB_USER as u inner join TB_CARE_RECEIVER as r where u.user_id = r.user_id';
-    // console.log('careRecvListSql', careRecvListSql());
+
+    let sql = sqlModule.careRecvListSql();
+    
     conn.query(sql, (err, rows)=>{
         // 마스킹 처리 함수
         let arrData = maskDatas(rows);

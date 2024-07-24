520ea693dc0fca9f6a94c92de6b03fbd41057124
ain-0308 <172338794+ain-0308@users.noreply.github.com>
2024-07-24 10:36:32 +0900

    아인_240724_10:36_careRecvRegRouter 코드 수정

--- nearcare/public/js/careRecvSqlModule.js
+++ nearcare/public/js/careRecvSqlModule.js
@@ -1,4 +1,4 @@
-const careRecvListSql = function careRecvListSql(sql){
+const careRecvListSql = function careRecvListSql(){
     let sql = 'select care_receiver_name, care_receiver_birth, care_receiver_gender, care_receiver_phone, user_email, care_receiver_level, care_receiver_add from TB_USER as u inner join TB_CARE_RECEIVER as r where u.user_id = r.user_id';
     return sql;
 };

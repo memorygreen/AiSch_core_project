0598aed2c25654b7ff973420f90864713d6b9e47
김진수 <80892395+subhun@users.noreply.github.com>
2024-07-25 10:10:32 +0900

    김진수 0725 10:10 sessionfile에러 수정

--- nearcare/app.js
+++ nearcare/app.js
@@ -9,25 +9,37 @@ const careRecvRegRouter = require('./routes/careRecvRegRouter');
 const chattingRouter = require('./routes/chattingRouter');
 const matchingRouter = require('./routes/matchingRouter');
 const csRouter = require('./routes/csRouter');
+const userRouter = require('./routes/userRouter');
 const path = require('path');
+const fs = require('fs');
 
 const session = require('express-session');
 const fileStore = require('session-file-store')(session);
+
+// 세션 파일 저장소 경로 설정
+const sessionDir = path.join(__dirname, 'sessions');
+
+// 세션 디렉토리 생성
+if (!fs.existsSync(sessionDir)) {
+    fs.mkdirSync(sessionDir, { recursive: true });
+}
+
 // 세션 설정
 app.use(session({
-    httpOnly : true, // http로 들어온 요청만 처리
-    resave : false, // 세션을 항상 재 저장할 건지 
-    secret : 'secret', // 암호화할 때 사용하는 키값 
-    store : new fileStore(), // 세션을 저장하기 위한 저장소 셋팅
-    saveUninitialized : false // 기본값은 true 세션에 저장할 내용이 없더라도 저장할 것인지
+    httpOnly: true, // http로 들어온 요청만 처리
+    resave: false, // 세션을 항상 재 저장할 건지 
+    secret: 'secret', // 암호화할 때 사용하는 키값 
+    store: new fileStore({ path: sessionDir }), // 세션을 저장하기 위한 저장소 셋팅
+    saveUninitialized: true // 기본값은 true 세션에 저장할 내용이 없더라도 저장할 것인지
 }));
+
 
 // 정적 파일요청 폴더 등록
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(bp.json()); // JSON 데이터 처리 등록, open ai api 사용 시 필요
 
 // post 데이터 처리 등록
-app.use(bp.urlencoded({extended : true}));
+app.use(bp.urlencoded({ extended: true }));
 
 // 메인 라우터 등록
 app.use('/', mainRouter);
@@ -35,13 +47,15 @@ app.use('/careRecvReg', careRecvRegRouter);
 app.use('/chatting', chattingRouter);
 app.use('/matching', matchingRouter);
 app.use('/cs', csRouter);
-
+app.use('/user', userRouter);
 
 // 넌적스 셋팅
 app.set('view engine', 'html');
 nunjucks.configure('views', {
-    express : app,
-    watch : true
+    express: app,
+    watch: true
 });
 
-app.listen(port);
+app.listen(port, () => {
+    console.log(`Server is running on port ${port}`);
+});
---
+++ nearcare/routes/userRouter.js
@@ -0,0 +1,9 @@
+const express = require("express");
+const router = express.Router();
+const conn = require("../config/db");
+const formatDate = require("../public/js/formatDate");
+
+
+
+
+module.exports = router;
